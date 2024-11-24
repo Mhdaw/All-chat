14107b9bb6e32320feb6258950b6b4201b443e76
Mahdi Seddigh <164439157+Mhdaw@users.noreply.github.com>
2024-11-24 22:44:48 +0330

    Update and rename text_2_speech.py to text2speech.py

--- backend/text_2_speech.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-
-language = "en"
-
-def generate_speech(text = None, language = "en", name = "conversation"):
-    myobj = gTTS(text=text, lang=language, slow=False)
-    # Saving the converted audio
-    myobj.save(f"{name}.mp3")
