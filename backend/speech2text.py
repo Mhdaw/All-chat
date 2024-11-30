ade4b15f0d31953e4e12dc41a8ad89c7ceb35c7c
Mahdi Seddigh <164439157+Mhdaw@users.noreply.github.com>
2024-12-01 00:15:50 +0330

    Update speech2text.py
    commented model option

--- backend/speech2text.py
+++ backend/speech2text.py
@@ -15,11 +15,11 @@ def load_transcribe_model(model_name):
     model.config.forced_decoder_ids = None
     return model, processor
 
-
-def transcribe_speech(audio_file, model_name):
+model, processor = load_transcribe_model(model_name)
+def transcribe_speech(audio_file): #, model_name
     """Transcribe speech from audio file"""
     try:
-        model, processor = load_transcribe_model(model_name)
+        #model, processor = load_transcribe_model(model_name)
         audio, sr = librosa.load(audio_file, sr=48000)
         audio_resampled = librosa.resample(audio, orig_sr=sr, target_sr=16000)
         input_features = processor(
