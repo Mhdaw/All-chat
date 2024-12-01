d73f7807b28db4a16d8d78af7654e901f78730e2
Mahdi Seddigh <164439157+Mhdaw@users.noreply.github.com>
2024-12-01 22:23:39 +0330

    Update pixtral.py

--- backend/pixtral.py
+++ backend/pixtral.py
@@ -3,35 +3,20 @@ from vllm import LLM
 from vllm.sampling_params import SamplingParams
 import logging
 
+# Globals for the model
+llm = None
 model_name = "mistralai/Pixtral-12B-2409"
 sampling_params = SamplingParams(max_tokens=8192)
-llm = None
 
 def load_pixtral_model():
+    """Loads the Pixtral model."""
     global llm
-    if torch.cuda.is_available():
-        logging.info(f"GPU is available. Loading the {model_name} model...")
-        llm = LLM(model=model_name, tokenizer_mode="mistral")
-        logging.info("Pixtral model loaded successfully.")
-    else:
-        logging.warning("GPU is not available. The model will not be loaded.")
-
-def get_pixtral_response(prompt, image_url):
     try:
-        if llm is None:
-            return None, None, "GPU is not available. Please try another service. Pixtral model only works with GPU device."
-
-        messages = [
-            {
-                "role": "user",
-                "content": [{"type": "text", "text": prompt}, {"type": "image_url", "image_url": {"url": image_url}}]
-            },
-        ]
-
-        outputs = llm.chat(messages, sampling_params=sampling_params)
-        response = outputs[0].outputs[0].text
-        return response, None, None
+        if torch.cuda.is_available():
+            logging.info(f"GPU is available. Loading the {model_name} model...")
+            llm = LLM(model=model_name, tokenizer_mode="mistral")
+            logging.info("Pixtral model loaded successfully.")
+        else:
+            logging.warning("GPU is not available. The model will not be loaded.")
     except Exception as e:
-        logging.error(f"Error getting pixtral response: {e}")
-        return None, None, "Failed to get the pixtral response."
-
+        logging.error(f"Error loading Pixtral model: {e}")
