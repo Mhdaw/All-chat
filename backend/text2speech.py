f4fcb9d750a29515a9d2b9e648c88fec4363fa64
Mahdi Seddigh <164439157+Mhdaw@users.noreply.github.com>
2024-12-12 18:48:49 +0330

    Update text2speech.py

--- backend/text2speech.py
+++ backend/text2speech.py
@@ -6,7 +6,13 @@ import logging
 import uuid
 from pydub import AudioSegment
 
+try:
+    from TTS.api import TTS
+
 language = "en"
+
+
+c_tts_name = "tts_models/multilingual/multi-dataset/xtts_v2"
 
 def generate_speech(text, language, AUDIO_FOLDER, model="gtts"):
     if model == "gtts":
@@ -26,12 +32,12 @@ def generate_speech(text, language, AUDIO_FOLDER, model="gtts"):
             logging.error("GPU is NOT available! Please try another model.")
             return None
         try:
-            from TTS.api import TTS
             filename = f"{uuid.uuid4()}.wav"
             filepath = os.path.join(AUDIO_FOLDER, filename)
-            tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
+            tts = TTS(c_tts_name).to("cuda")
             # generate speech by cloning a voice using default settings
             tts.tts_to_file(text=text, file_path=filepath, speaker="Ana Florence", language="en", split_sentences=True)
+            logging.info("speech is generated with coquiTTS")
             audio = AudioSegment.from_wav(filepath)
             filename_mp3 = f"{uuid.uuid4()}.mp3"
             filepath_mp3 = os.path.join(AUDIO_FOLDER, filename_mp3)
